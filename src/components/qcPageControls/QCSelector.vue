0476306b7fbd56ce4702c663c364fdd28b7e4726
Jeff Holtzman <jkholtz@umich.edu>
2025-01-27 13:00:30 -0500

    cleaning up qcSelector (study dropdown)

--- src/components/qcPageControls/QCSelector.vue
+++ src/components/qcPageControls/QCSelector.vue
@@ -3,7 +3,7 @@
     @update:modelValue="onSelectUpdate"
     class="mt-3"
     :items="qcStore.studyList"
-    v-model="selectedStudy"
+    v-model="qcStore.selectedStudyName"
     density="compact"
     :label="controlSet.caption"
     bg-color="white"
@@ -14,22 +14,9 @@
 </template>
 
 <script setup>
-import { ref, watch } from 'vue'
-import { PAGE_NAMES } from '@/constants'
 import { useQCStore } from '@/stores/QCStore'
 
 const qcStore = useQCStore()
-
-const qcPage = PAGE_NAMES.QC
-
-const selectedStudy = ref('')
-
-// this is necessary to populate the selected study on initial load
-watch(() => qcStore.studyList, async () => {
-  const initialStudy = qcStore.studyList[0]
-  qcStore.selectedStudy = initialStudy
-  selectedStudy.value = initialStudy
-})
 
 const { controlSet } = defineProps({
   controlSet: {}
